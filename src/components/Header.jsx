f1c4297d361b902d02d5e0b27fcecf5498dba3ab
muha <1student.muhammed1@gmail.com>
2024-05-06 20:42:38 +0300

    feat: use buttons to navigate between routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,43 @@
+import { useNavigate } from 'react-router-dom';
 
-export default function Header() {
+function AboutButton() {
+    let history = useNavigate();
+    const handleClick = () => {
+        history('/about');
+    }
+
     return (
-        <header className= "mt-6 md:ml-20  text-lg max-md:ml-12 max-sm:text-md max-sm:mt-8 quicksand-font ">
+      <button type="button" onClick={handleClick}>
+        About
+      </button>
+    );
+}
+
+function PostsButton() {
+    let history = useNavigate();
+    const handleClick = () => {
+        history('/posts');
+    }
+
+    return (
+      <button type="button" onClick={handleClick}>
+        Posts
+      </button>
+    );
+}
+export default function Header() {
+
+
+    return (
+        <header className="mt-6 md:ml-20  text-lg max-md:ml-12 max-sm:text-md max-sm:mt-8 quicksand-font ">
             <nav>
                 <ul className="list-none flex  gap-16">
-                    <li className=" text-white ">Posts</li>
-                    <li className="  text-white "> About</li>
-
+                    <li className=" text-white ">
+                        <PostsButton />
+                    </li>
+                    <li className="  text-white ">
+                        <AboutButton />
+                    </li>
                 </ul>
             </nav>
       </header>
