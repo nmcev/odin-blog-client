ec793153dbad1d59da57515fa9626f95467803b7
muha <1student.muhammed1@gmail.com>
2024-05-06 20:46:21 +0300

    feat: use the outlet for dynamic content rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import 'axios'
-import Header from './components/Header'
-import { SpotlightPreview } from './components/SpotlightPreview'
-import { Spotlight } from './components/Spotlight'
-
-
-
+import { Outlet } from 'react-router-dom'
 function App() {
-  return (
-    <div className="h-dvh w-full  md:items-center md:justify-center bg-gray-900 antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
-      <Header />
-      <SpotlightPreview /> 
-      
 
-</div>
-    
+
+  return (
+    <div className=' w-full max-md:mt-40 antialiased bg-grid-white/[0.02] relative overflow-hidden'>
+
+      <Outlet />
+    </div>
   )
 }
 
