813dd072187cafb9c650a3fe5e3835cd010ed3f0
muha <1student.muhammed1@gmail.com>
2024-05-06 20:43:17 +0300

    feat: set basic routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './components/HomePage.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children:[
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "posts",
+        element: <div></div>,
+      },
+      {
+        path: "about",
+        element: <div></div>,
+      }
+    ]
+  },
+  
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
