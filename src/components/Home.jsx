2e40e5c84e0b00c32f64ecf549f1a3f493e05e70
muha <1student.muhammed1@gmail.com>
2024-05-07 19:13:07 +0300

    style: change background color of the entry page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import Header from "./Header";
 export function Home() {
   return (
 
-      <div className=' w-full flex flex-col bg-gray-900 antialiased bg-grid-white/[0.02] relative overflow-hidden'>
+      <div className=' w-full flex flex-col bg-[#153448] antialiased bg-grid-white/[0.02] relative overflow-hidden '>
         <Header />
 
-      <div className=" p-4 max-w-7xl flex flex-col h-[100dvh] ] justify-center items-center  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className=" p-4 max-w-7xl flex flex-col h-[100dvh]  justify-center items-center  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <div className="flex flex-col items-center justify-center mb-12 max-sm:mb-20">
           <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 font-poppins uppercase">
             Welcome to the Blog
