e0dd684afe3f859049d24147f7a3648118f5417b
muha <1student.muhammed1@gmail.com>
2024-05-07 22:58:19 +0300

    feat: import the footer in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
 function App() {
 
 
@@ -6,6 +7,7 @@ function App() {
     <div className=' w-full antialiased bg-grid-white/[0.02] relative overflow-hidden'>
 
       <Outlet />
+      <Footer />
     </div>
   )
 }
