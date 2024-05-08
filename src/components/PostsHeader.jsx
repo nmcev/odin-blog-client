45c7efe697b817b03ef0519c2660e18ef440af71
muha <1student.muhammed1@gmail.com>
2024-05-08 16:36:31 +0300

    build: posts pages' header

---
+++ src/components/PostsHeader.jsx
@@ -0,0 +1,10 @@
+
+export default function PostsHeader() {
+    return (
+    <div className="container mx-auto py-8">
+        <h1 className="text-4xl text-center text-black font-boldPoppins">
+            Posts
+        </h1>
+    </div>
+  )
+}
