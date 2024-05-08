3fc61c6837f5514d679497b88d7a10ff7b556510
muha <1student.muhammed1@gmail.com>
2024-05-08 16:37:26 +0300

    feat: use posts header and post card in posts page

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,14 +1,22 @@
 import Header from '../components/Header'
+import PostCard from '../components/PostCard'
+import PostsHeader from '../components/PostsHeader'
 export default function PostsPage() {
-  return (
-
-      <div className=' w-full antialiased bg-grid-white/[0.02] relative overflow-hidden'>
-          <Header color='black'/>
-          <div className=" p-4 max-w-7xl flex flex-col   justify-center items-center  mx-auto relative z-10  w-full pt-20 md:pt-0">
-              <h1>
-                    Posts Page
-              </h1>
-          </div>
-          </div>
-          )
+    return (
+        <>
+        <Header color='black'/>
+      <section className='container min-h-screen mx-auto'>
+            <PostsHeader />
+             <div className='flex divide-y-2 flex-col items-center w-full max-w-3xl mx-auto'>
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                </div>
+            </section>
+        </>
+    )
 }
