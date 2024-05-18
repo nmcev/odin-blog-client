4172ee0851cd34be5d574ce057bef947fccd2986
muha <1student.muhammed1@gmail.com>
2024-05-18 14:26:56 +0300

    feat: use posts' data on postCard component

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,20 +1,26 @@
+import PropTypes from 'prop-types'
+import { useNavigate } from 'react-router'
+export default function PostCard({ post }) {
+    const navigate = useNavigate();
 
-export default function PostCard() {
     return (
-        <article className="flex w-full p-4 pb-6">
+        <article className="flex w-full p-4 pb-6 cursor-pointer" onClick={() => navigate(`/posts/${post.slug}`)}>
             {/* information section */}
             <div className="p-4 w-full">
-                <p className="text-sm text-neutral-600">2 May 2024</p>
-                <h1 className="text-2xl font-bold text-neutral-900">Post Title</h1>
-                <p className="text-sm text-neutral-600 mt-[4px]">
-                    How to create a blog using React and Tailwind CSS
-                    in 2024, with latest and now I am building this app for learning how to build an API
+                <p className="text-sm text-neutral-500">{post.postedTime}</p>
+                <h1 className="text-2xl  font-bold text-neutral-900 font-boldPoppins">{post.title}</h1>
+                <p className='text-sm font-lightPoppins text-neutral-700'>
+                    {post.description}
                 </p>
             </div>
             {/*  image section */}
-            <div className="flex items-center">
-                <img className="max-w-full h-auto" src="https://dummyimage.com/128x128" alt="blog" width={128} height={128}/>
+            <div className="flex items-center mx-auto">
+                <img className="max-w-full h-auto min-w-48" src={post.img} alt="blog" width={328} height={328}/>
             </div>
         </article>
   )
+}
+
+PostCard.propTypes = {
+    post: PropTypes.object.isRequired,
 }
