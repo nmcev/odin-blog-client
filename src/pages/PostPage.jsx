3974729ba2f8cd3c9045cbe28ee05919fdd6a0c5
muha <1student.muhammed1@gmail.com>
2024-05-18 21:18:09 +0300

    style: remove the dark theme of the loading dots

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -58,7 +58,7 @@ export default function PostPage() {
     return (
       <>
       <Header color='black' />
-      <div className='flex  space-x-2 justify-center items-center bg-white h-[90vh] dark:invert'>
+      <div className='flex  space-x-2 justify-center items-center bg-white h-[90vh]'>
       <span className='sr-only'>Loading...</span>
       <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
       <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
