346aeb4fa71335f47b915f81cc1b0c381c5cff09
muha <1student.muhammed1@gmail.com>
2024-05-18 21:03:16 +0300

    style: enhance comment style

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Comment({comment}) {
     return (
-         <div className="flex flex-row items-center w-full pb-2 border-b border-gray-400">
+         <div className="flex flex-row items-center w-full pb-2 border-b border-gray-300">
           <div className="w-10 h-10 rounded-full overflow-hidden flex self-start">
             <img
               src="https://odin-blog-bucket.s3.eu-north-1.amazonaws.com/3683ee82d45b76e04341886e87616e8c"
@@ -11,12 +11,12 @@ export default function Comment({comment}) {
             />
           </div>
 
-          <div className="pl-2 flex flex-col gap-2">
-            <div>
-              <h3 className="m-0 font-semibold">{comment.username}</h3>
-                        <small className="m-0">{comment.formattedDate}</small>
+          <div className="pl-2 flex flex-col gap-3">
+            <div className='flex  flex-col'>
+              <h3 className="m-0 font-boldPoppins">{comment.username}</h3>
+              <small className="m-0 text-xs">{comment.formattedDate}</small>
             </div>
-            <p className="text-base">
+            <p className="text-sm font-careem">
                 {comment.text}
             </p>
           </div>
