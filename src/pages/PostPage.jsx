546fd4d6ab603ace35ed589e7bced10e9632e159
muha <1student.muhammed1@gmail.com>
2024-05-18 14:58:34 +0300

    feat: update post page to load posts' data

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,23 @@
-import Header from '../components/Header'
+import { useParams } from 'react-router'
+import Header from '../components/Header';
+import { useContext } from 'react';
+import { PostsContext } from '../context/PostsContext';
 export default function PostPage() {
+  const { slug } = useParams();
+  const { posts } = useContext(PostsContext);
+
+  const post = posts.find(post => post.slug === slug);
+
+  if (!post) {
+    return (
+      <div className='flex  space-x-2 justify-center items-center bg-white h-[600px] dark:invert'>
+      <span className='sr-only'>Loading...</span>
+      <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+      <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+      <div className='h-4 w-4 bg-black rounded-full animate-bounce'></div>
+  </div>
+    )
+  }
   return (
     <>
       <Header color='black' />
@@ -7,37 +25,24 @@ export default function PostPage() {
 
       <article className="flex flex-col items-center justify-center max-sm:mx-2">
         <div className="mt-8 flex flex-col items-left ">
-          <h1 className="text-3xl font-bold font-careem ">
-            How to create a blog using React and Tailwind CSS
+            <h1 className="text-3xl font-bold font-careem ">
+              {post.title}
           </h1>
-          <p className="text-sm text-neutral-600 mt-[4px]">2 May 2024</p>
+            <p className="text-sm text-neutral-600 mt-[4px]">{post.postedTime}</p>
 
 
         <div>
             <p className="text-sm text-neutral-500 font-lightPoppins">
-              description of the post, Now I am building this app for learning how to build an API
+              {post.description}
           </p>
         </div>
 
         <div className="mt-8">
-          <img className="" src="https://dummyimage.com/804x492" alt="blog" width={804} height={492} />
+          <img  src={post.img} alt="blog" style={{ maxWidth: '800px', height: 'auto' }} />
          </div>
 
           <div className="mt-8 max-w-5xl">
-            <p className="text-lg text-neutral-900 font-careem li">
-              How to create a blog using React and Tailwind CSS in 2024, with latest and now I am building this app for learning how to build an API
-
-              this it the content of the post
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex praesentium rerum molestiae quos cum possimus numquam perferendis fugiat obcaecati commodi animi dolorum incidunt consequuntur, iste totam, necessitatibus vel consequatur veritatis?
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, hic? Totam incidunt facilis laudantium recusandae delectus. Doloremque odit labore molestiae adipisci error? Tempore maxime voluptas, dolores omnis sed molestiae numquam.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quasi perspiciatis rerum architecto, temporibus nostrum delectus quas et doloremque provident ea hic, suscipit veritatis quod exercitationem adipisci sed voluptatem porro.
-              {/* fill this with random text */}
-
-              random text: Lorem ipsum dolor sit amet consectetur adipisicing elit
-              . Sunt, quod. Quo, laboriosam
-              more random text: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quod. Quo, laboriosam
-              more random text: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quod. Quo, laboriosam
-
+            <p className="text-lg text-neutral-900 font-careem li" dangerouslySetInnerHTML={{__html: post.content}}>
             </p>
             </div>
         </div>
