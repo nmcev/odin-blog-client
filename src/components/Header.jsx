a13eea1a5bcdd202ddcee667c936bfc430afcb7a
muha <1student.muhammed1@gmail.com>
2024-05-20 19:33:28 +0300

    feat: add home button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { MdHome } from 'react-icons/md';
 
 function AboutButton() {
     let history = useNavigate();
@@ -26,6 +27,20 @@ function PostsButton() {
       </button>
     );
 }
+
+function HomeButton() {
+    let history = useNavigate();
+    const handleClick = () => {
+        history('/');
+    }
+
+    return (
+        <button type="button" onClick={handleClick}>
+          <MdHome />
+        </button>
+      );
+
+}
 export default function Header({color = 'white'}) {
 
     const colorVariants = {
@@ -35,7 +50,10 @@ export default function Header({color = 'white'}) {
     return (
         <header className="max-sm:pt-0 pt-8 md:ml-20  text-lg max-md:ml-12 max-sm:text-md max-sm:mt-8 font-careem ">
             <nav>
-                <ul className="list-none flex  gap-16">
+                <ul className="list-none flex  items-center gap-16">
+                    <li className={`${colorVariants[color]}`}>
+                        <HomeButton />
+                    </li>
                     <li className={`${colorVariants[color]}`} >
                         <PostsButton />
                     </li>
