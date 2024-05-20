58a651b82552e980994d18500940cc7e4eda8ad2
muha <1student.muhammed1@gmail.com>
2024-05-20 19:11:10 +0300

    style: increase the height of the loadings' div

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ export default function HomePage() {
         return (
             <>
         <Header color='black' />
-            <div className='flex  space-x-2 justify-center items-center bg-white h-[600px] '>
+            <div className='flex  space-x-2 justify-center items-center bg-white h-[800px] '>
                     <span className='sr-only'>Loading...</span>
                     <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
                     <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
