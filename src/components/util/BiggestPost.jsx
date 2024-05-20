72801457ede8b2542741b5220b648fbe88b94f98
muha <1student.muhammed1@gmail.com>
2024-05-20 19:28:50 +0300

    refactor: move the clickable div to another div

--- src/components/util/BiggestPost.jsx
+++ src/components/util/BiggestPost.jsx
@@ -6,8 +6,8 @@ export default function BiggestPost({ post }) {
     const navigate = useNavigate()
     return (
         <>
-            <div className="col-span-1 max-sm:mx-4 cursor-pointer" onClick={() => navigate(`/posts/${post.slug}`)} >
-                <div className="flex flex-col">
+            <div className="col-span-1 max-sm:mx-4 " >
+                <div className="flex flex-col cursor-pointer" onClick={() => navigate(`/posts/${post.slug}`)}>
                 <img src={post.img} alt="myImage" className="w-full h-auto max-md:max-w-full" />
                     <h4 className="text-lg font-boldPoppins text-neutral-800 mt-4">{post.title}</h4>
                     <p className="text-sm font-lightPoppins text-neutral-900">{post.description}</p>
