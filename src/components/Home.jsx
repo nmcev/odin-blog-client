a49eb774448348e7739ed0fcb9d601aa8364d944
muha <1student.muhammed1@gmail.com>
2024-05-21 16:00:26 +0300

    style: add dark class color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,11 +11,11 @@ export function Home() {
 
       <div className=" p-4 max-w-7xl flex flex-col h-[100dvh]  justify-center items-center  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <div className="flex flex-col items-center justify-center mb-12 max-sm:mb-20">
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 font-poppins uppercase">
+          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b dark:from-white from-neutral-50 dark:to-slate-50 to-neutral-400 bg-opacity-50 font-poppins uppercase">
             Welcome to the Blog
             <br /> from the future.
         </h1>
-          <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto font-lightPoppins">
+          <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center dark:from-white  dark:to-slate-50 mx-auto font-lightPoppins">
             A blog about sharing my experiences and journey as a web developer and software engineer student.
           </p>
         </div>
